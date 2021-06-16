29b42e85d5828984d2c8c41d2df573c6917e3ba2
theo m <theo@mantz.io>
2021-06-16 16:45:39 -0400

    work on splitter class

--- src/preprocessing/sentenceSplitter.js
+++ src/preprocessing/sentenceSplitter.js
@@ -3,11 +3,47 @@ const Splitter = require("./Splitter");
 
 // RegExp Patterns
 const newLine = /((?:\r?\n|\r)+)/;
-
+const clean = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+const punctuation = /[,-./]/g;
 class SentenceSplitter extends Splitter {
   newLine(str) {
     return str.split(newLine);
   }
+
+  punctuation(str) {
+    return str.split(punctuation);
+  }
+
+  clean(str) {
+    let result = str.replace(clean, "");
+    return result.replace(newLine, "");
+  }
+
+  sentences(str) {
+    const { newLine, clean, punctuation } = this;
+    let arr = newLine(str);
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+      let s = arr[i].trim();
+      if (s !== "") {
+        let arr2 = punctuation(s);
+        for (let j = 0; j < arr2.length; j++) {
+          let s2 = arr2[j].trim();
+          if (s2 !== "") result.push(clean(s2));
+        }
+      }
+    }
+    return result;
+  }
 }
 
 module.exports = SentenceSplitter;
+const input =
+  "This string is a bit harder. \r \r It has some extra stuff in it \n so there's a good way to normalize; and split               ";
+
+function main(s) {
+  const splitter = new SentenceSplitter();
+  return splitter.sentences(s);
+}
+
+console.log(main(input));
--- test/preprocessing/sentenceSplitter.test.js
+++ test/preprocessing/sentenceSplitter.test.js
@@ -5,11 +5,12 @@ const SentenceSplitter = require("../../src/preprocessing/SentenceSplitter");
 const inputString =
   "This is some sample text. It has a few lines. Some lines are really long... And others, are quite short. I think if we keep going we should cover all of our bases here but just in case.";
 
-const splitInput = [
-  "This is some simple text",
+const outputString = [
+  "This is some sample text",
   "It has a few lines",
   "Some lines are really long",
-  "And others, are quite short",
+  "And others",
+  "are quite short",
   "I think if we keep going we should cover all of our bases here but just in case",
 ];
 
@@ -26,20 +27,43 @@ const complexOutput = [
   " so there's a good way to normalize; and split               ",
 ];
 
+const complexOutput2 = [
+  "This string is a bit harder",
+  "It has some extra stuff in it",
+  "so theres a good way to normalize and split",
+];
+
 describe("sentenceSplitter", () => {
   let splitter;
+
   beforeAll(() => {
     splitter = new SentenceSplitter();
-    return splitter;
   });
-  describe("It should not split strings at punctuation", () => {
-    test("String without newline separators", () => {
-      expect(splitter.newLine(inputString)).toEqual([inputString]);
+
+  describe("The newLine method", () => {
+    describe("It should not split strings at punctuation", () => {
+      test("String without newline separators", () => {
+        expect(splitter.newLine(inputString)).toEqual([inputString]);
+      });
+    });
+    describe("It should split strings at newline separators", () => {
+      test("String with newline separators", () => {
+        expect(splitter.newLine(complexInputString)).toEqual(complexOutput);
+      });
     });
   });
-  describe("It should split strings at newline separators", () => {
-    test("String with newline separators", () => {
-      expect(splitter.newLine(complexInputString)).toEqual(complexOutput);
+  describe("The sentence method", () => {
+    describe("It should split and sanitize inputs", () => {
+      test("Regular input", () => {
+        expect(splitter.sentences(inputString)).toEqual(outputString);
+      });
+      describe("A bit more difficult input", () => {
+        test("Another input", () => {
+          expect(splitter.sentences(complexInputString)).toEqual(
+            complexOutput2
+          );
+        });
+      });
     });
   });
 });
